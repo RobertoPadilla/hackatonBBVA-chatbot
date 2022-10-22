c1e6568e9324edea35a5da80de0933133980d6da
Roberto Padilla (Linux) <basshivan981@gmail.com>
2022-10-21 22:35:06 -0500

    feat: send message added

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-from fastapi import FastAPI
+from email import message
+from fastapi import FastAPI, Request
+from twilio_client import client as twilio_client
+from pydantic import BaseModel
+import requests
+
+class SendMessageIn(BaseModel):
+    body: str
+    to: str
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "API Blue Fireflies"}
+
+
+@app.post("/send_message")
+async def send_message(message: SendMessageIn):
+    response = twilio_client.messages.create(
+        body=message.body,
+        from_='whatsapp:+14155238886',
+        to=f'whatsapp:{message.to}'
+    )
+
+    return {"message": response.sid}
+
+@app.post("/callback")
+async def callback(request: Request):
+    # message = {"to": "whatsapp:+5215582403665"}
+    message = {}
+    
+    for param in request.query_params:
+        message['body'] = param
+        print(message)
+    params = request.query_params
+    return {"message": params}
---
+++ twilio_client.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+load_dotenv()
+
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+client = Client(account_sid, auth_token)
